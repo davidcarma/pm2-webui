81ba6bbf3ece8d9a81b92b61830898222acb5cc5
David Saliba <davidcarma@users.noreply.github.com>
2023-07-24 18:58:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -133,6 +133,6 @@ app.get('/', function(req, res) {
     });
 });
 
-app.listen(3000, function() {
-    console.log('App is listening on port 3000');
+app.listen(3333, function() {
+    console.log('App is listening on port 3333');
 });
