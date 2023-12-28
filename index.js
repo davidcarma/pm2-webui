fb25e4acad214e94184d3cc3888e307794302b6d
davidcarma <david.saliba@carma.com>
2023-12-28 05:45:56 +0100

    added logs and ip whitelist

--- index.js
+++ index.js
@@ -1,10 +1,63 @@
 const express = require('express');
 const path = require('path');
-
-const pmx = require('pmx');
+const pmx = require('pmx').init(); // Initialize PMX
 const pm2 = require('pm2');
+const chalk = require('chalk');
 
 const app = express();
+
+
+// Variable to store the IP whitelist, initialized as null
+let ipWhitelist = null;
+
+// Fetch configuration using PM2 Keymetrics
+pmx.initModule({}, (err, conf) => {
+    if (conf.IP_WHITELIST) {
+        console.log  ("Configuration :");
+        console.table (conf);
+        ipWhitelist = conf.IP_WHITELIST.split(',');
+    } else {
+        console.warn(chalk.yellow('Warning: IP whitelist not configured. \n\r Set the whitelist using `pm2 set pm2-webui:IP_WHITELIST "127.0.0.1,192.168.1.1"`'));
+    }
+});
+
+// Custom logging middleware
+app.use((req, res, next) => {
+    // Capture the start time
+    const startTime = process.hrtime();
+
+    // Function to log details
+    const logDetails = () => {
+        const diff = process.hrtime(startTime);
+        const responseTime = diff[0] * 1e3 + diff[1] * 1e-6; // Convert to milliseconds
+
+        console.log(`Path: ${req.path} | IP: ${req.ip} | X-Forwarded-For: ${req.headers['x-forwarded-for'] || 'N/A'} | Status: ${res.statusCode} | Response Time: ${responseTime.toFixed(3)} ms`);
+    };
+
+    // Hook into the finish event of the response
+    res.on('finish', logDetails);
+
+    // Proceed to the next middleware
+    next();
+});
+
+
+
+
+// Middleware for IP Whitelisting
+app.use((req, res, next) => {
+    if (!ipWhitelist) {
+        return next(); // Allow all IPs if whitelist not configured
+    }
+
+    const clientIp = req.ip || req.connection.remoteAddress;
+    if (ipWhitelist.includes(clientIp)) {
+        next();
+    } else {
+        res.status(403).send('Access Denied');
+    }
+});
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
